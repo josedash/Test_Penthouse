553537dff8c18a8f872b5c84f3cc53b8d5a45011
Jose Martinez <josemtzmo.91@gmail.com>
2020-03-29 15:08:11 -0400

    test_data

--- TrialSuite/src/test/java/stepdefs/GlobalHooks.java
+++ TrialSuite/src/test/java/stepdefs/GlobalHooks.java
@@ -5,7 +5,6 @@ import io.cucumber.java.Scenario;
 import utils.test.cucumberReport.CucumberUtils;
 
 import java.lang.management.ManagementFactory;
-import java.sql.SQLOutput;
 
 public class GlobalHooks {
 
@@ -13,7 +12,9 @@ public class GlobalHooks {
     @Before
     public void setUp(Scenario scenario) {
         System.out.println("set UP");
+        System.out.println(scenario.getId() + scenario.getName());
         CucumberUtils.scenario = scenario;
+
         long threadId = Thread.currentThread().getId();
         String processName = ManagementFactory.getRuntimeMXBean().getName();
         System.out.println("Started in thread: " + threadId + ", in JVM: " + processName);
